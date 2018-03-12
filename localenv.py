1b75f4427d3992a27be4482f209584f250c5e638
Dolly <dye1020@gmail.com>
2018-03-11 18:25:19 -0700

    update

---
+++ localenv.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "lLH29a5xJkJEzZhKCJncKqjyI"
+consumer_secret = "nRH4ZLlgmJ4SltYanAbUKWWtY2xEQiU5iXo2cdUJYFBAn61Vd8"
+access_token = "3084688655-pWoWXIMBiUKsTMjZAQHBN9Vw70hPXhsDqCfw7ck"
+access_token_secret = "G5absx1xRVvWIC3tUShsWGAraW8kJx15NTNtf8PoFuJOM"
